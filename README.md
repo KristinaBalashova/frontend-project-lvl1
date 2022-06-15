a0dc3229701bb755df754c90005b143d19a841f0
KristinaBalashova <infernalmajesty1350@gmail.com>
2022-06-15 13:11:31 +0300

    added even-game

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 [![Actions Status](https://github.com/KristinaBalashova/frontend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/KristinaBalashova/frontend-project-lvl1/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
 ![example workflow](https://github.com/KristinaBalashova/frontend-project-lvl1/actions/workflows/makelint.yml/badge.svg)
+Link to ascinema of even-game https://asciinema.org/a/t0wYpZ3XYfKPw6ZGMhIjIYl1B
---
+++ src/even.js
@@ -0,0 +1,34 @@
+import readlineSync from 'readline-sync';
+
+export const evenGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello,${name}`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const isNumberEven = (num) => {
+    if (num % 2 === 0) {
+      return 'yes';
+    }
+    return 'no';
+  };
+  const gameEnd = `Sorry, you need to answer "yes" or "no". End of the game. Let us try again, ${name}!`;
+  const wrongAnswerNo = `"no" is wrong answer ;(. Correct answer was "yes". Let us try again, ${name}!`
+  const wrongAnswerYes = `"yes" is wrong answer ;(. Correct answer was "no". Let us try again, ${name}!`;
+
+  for (let i = 3; i > 0; i -= 1) {
+    const randomNumber = Math.floor(Math.random() * 100);
+    console.log(`Question:${randomNumber}`);
+    const userAnswer = readlineSync.question('Your answer:');
+    if (userAnswer !== 'yes' && userAnswer !== 'no') {
+      return console.log(gameEnd);
+    }
+
+    if (isNumberEven(randomNumber) === userAnswer) {
+      console.log('Correct!');
+    } else if (isNumberEven(randomNumber) !== userAnswer && userAnswer === "yes") {
+      return console.log(wrongAnswerYes);
+    }
+      else return console.log(wrongAnswerNo);
+  }
+  return console.log(`Congratulations, ${name}!`);
+};
